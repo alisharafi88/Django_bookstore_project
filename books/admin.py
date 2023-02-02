0b4311816b7ccb65263ad96d1b24ada5d54b4a9d
alisharafi88 <alisharafi893@gmail.com>
2023-02-02 09:19:18 +0330

    add comment model for books

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Comment
+
+
+@admin.register(Comment)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'datetime', 'text')
 
 
 admin.site.register(Book)
--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -15,3 +16,9 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book_detail', args=[self.id])
 
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comment')
+    text = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
